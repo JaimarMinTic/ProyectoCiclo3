2daabd17132f45b1de818c02ad1a5375f08f0fc0
JaimarMinTic <112777891+JaimarMinTic@users.noreply.github.com>
2022-09-17 08:39:56 -0500

    Add files via upload

---
+++ Home/dinamicoHome.js
@@ -0,0 +1,19 @@
+var user = document.formHome.UserName;
+var contra = document.formHome.Password
+let log_in = document.getElementById(loginButton);
+ log_in.onclick = alert("hola");
+// log_in.addEventListener("click", alert("nose"));
+
+
+// document.buttonLogin.onclick= alert("y ahora");
+
+// if(user.value.length>1){
+//     alert("si")
+// }
+// else{alert("no")}
+// var user_len = user.value.length;
+// if(user_len==0 || user_len<8){
+//     alert("debes ingresar un username valido de min 8 caracteres")
+//     document.formHome.Username.focus();
+// }
+// alert("bienvenido");
